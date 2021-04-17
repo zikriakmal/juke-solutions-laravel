c45d8230fcf9ec65ee980e816c1cc2b2bfa3a164
zikri <zikriakmale@gmail.com>
2021-04-17 08:50:50 +0700

    initial commit with migrations

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('dob');
+            $table->integer('province_id');
+            $table->integer('city_id');
+            $table->string('street');
+            $table->string('ktp_number');
+            $table->string('uri_ktp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
