80ac5f5fe0cf3f99bcb58d5ae9a4ed25895dbe2d
zikri <zikriakmale@gmail.com>
2021-04-18 10:37:23 +0700

    fixing typography

--- resources/views/main-page/include/modal.blade.php
+++ resources/views/main-page/include/modal.blade.php
@@ -45,7 +45,7 @@
      <div class="modal-dialog modal-lg">
          <div class="modal-content">
              <div class="modal-header">
-                 <h5 class="modal-title" id="exampleModalLabel">Add New Employee</h5>
+                 <h5 class="modal-title" id="exampleModalLabel">Update Employee Data</h5>
                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
              </div>
              <form action="{{ route('employee.update', 0) }}" method="post" enctype="multipart/form-data">
